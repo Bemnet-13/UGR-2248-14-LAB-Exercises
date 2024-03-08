507c38878d603b62de72218e86ecbc214f07fd40
Bemnet-13 <bemnetaschalewwodaj@gmail.com>
2024-03-08 14:42:07 +0300

    Exercise -1 --Variables and Data types

---
+++ exercise1.dart
@@ -0,0 +1,9 @@
+// Exercise -1 --Variables and Data types
+void main() {
+  String name = 'Bemnet Aschalew';
+  int age = 20;
+  String favColor = 'Black';
+
+  print(
+      'My name is ${name}. I am ${age} years old. My favorite color is ${favColor}.');
+}
