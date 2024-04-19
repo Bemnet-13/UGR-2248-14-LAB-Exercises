3e9790a2a354c948adda98ea500f7e53851a44c2
Bemnet-13 <bemnetaschalewwodaj@gmail.com>
2024-04-19 15:31:04 +0300

    State management with Provider

--- counter_modified/lib/main.dart
+++ counter_modified/lib/main.dart
@@ -1,48 +1,91 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => CountChanger(),
+      child: MyApp(),
+    ),
+  );
 }
+
+class CountChanger extends ChangeNotifier {
+  int count = 0;
+
+  void increaseCount() {
+    count++;
+    notifyListeners();
+  }
+  void decreaseCount() {
+    count--;
+    notifyListeners();
+  }
+}
+
 
 class MyApp extends StatelessWidget {
   static int count = 0;
+
   const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text("Counter"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$count',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
         ),
-      ),
+        home: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Theme
+                .of(context)
+                .colorScheme
+                .inversePrimary,
+            title: Text("Counter"),
+          ),
+          body: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text(
+                  'You have pushed the button this many times:',
+                ),
+                Consumer<CountChanger>(
+                  builder: (context, counter, child) {
+                    return Text(
+                      '${counter.count}',
+                    );
+                  },
+                ),
+                Text(
+                  '$count',
+                  style: Theme
+                      .of(context)
+                      .textTheme
+                      .headlineMedium,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(onPressed: (){
+                      Provider.of<CountChanger>(context, listen: false).increaseCount();
+                    },
+                        child: Icon(Icons.minimize)),
+                    ElevatedButton(onPressed: (){
+                      Provider.of<CountChanger>(context, listen: false).decreaseCount();
+                    },
+                        child: Icon(Icons.add))
+                  ],
+                )
+              ],
+            ),
+          ),
 
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-
+        )
     );
   }
-
+}
