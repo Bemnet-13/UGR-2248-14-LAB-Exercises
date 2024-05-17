dcb42d332b2b90512262444c6974fc19b8886f91
Bemnet-13 <bemnetaschalewwodaj@gmail.com>
2024-05-17 15:01:11 +0300

    Kotline Ecercise- Triangle

---
+++ Kotlin_Exercises/triangle.kt
@@ -0,0 +1,13 @@
+fun triangle(side1: Double, side2 : Double, side3 : Double) {
+
+    
+    val answer = when {
+        ((side1 == side2) && (side2 == side3)) -> "Equilateral"
+        ((side1 == side2) || (side2 == side3) || (side1 == side3)) -> "Isosceles"
+        else -> "Scalene"
+    } 
+    println("The triangle is $answer")
+}
+fun main() {
+triangle(2.0,2.0,3.0)
+}
