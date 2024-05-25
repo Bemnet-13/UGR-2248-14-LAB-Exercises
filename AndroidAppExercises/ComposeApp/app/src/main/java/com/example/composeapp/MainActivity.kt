46905b52095fd532f0453b2a0909e2810d487961
Bemnet-13 <bemnetaschalewwodaj@gmail.com>
2024-05-25 11:27:12 +0300

    Lesson2- Image

--- AndroidAppExercises/ComposeApp/app/src/main/java/com/example/composeapp/MainActivity.kt
+++ AndroidAppExercises/ComposeApp/app/src/main/java/com/example/composeapp/MainActivity.kt
@@ -3,13 +3,16 @@ package com.example.composeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.composeapp.ui.theme.ComposeAppTheme
 
@@ -26,11 +29,18 @@ data class Message(val author:String, val body : String)
 
 @Composable
 fun MessageCard(msg : Message) {
-    Column {
-        Text(msg.author)
-        Text(msg.body)
+    Row {
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_foreground),
+            contentDescription = "Android Image"
+        )
+        Column {
+            Text(msg.author)
+            Text(msg.body)
+        }
     }
 }
+
 
 @Preview
 @Composable
