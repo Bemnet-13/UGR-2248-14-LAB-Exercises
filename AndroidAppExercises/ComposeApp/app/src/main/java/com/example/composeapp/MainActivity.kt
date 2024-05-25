d45dc6fec57b9de0a5bc3a8dd1b3f883261c38f1
Bemnet-13 <bemnetaschalewwodaj@gmail.com>
2024-05-25 11:35:26 +0300

    Lesson2-Composables

--- AndroidAppExercises/ComposeApp/app/src/main/java/com/example/composeapp/MainActivity.kt
+++ AndroidAppExercises/ComposeApp/app/src/main/java/com/example/composeapp/MainActivity.kt
@@ -6,14 +6,21 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composeapp.ui.theme.ComposeAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,11 +36,15 @@ data class Message(val author:String, val body : String)
 
 @Composable
 fun MessageCard(msg : Message) {
-    Row {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
         Image(
             painter = painterResource(id = R.drawable.ic_launcher_foreground),
-            contentDescription = "Android Image"
+            contentDescription = "Android Image",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
         )
+        Spacer(modifier = Modifier.width(8.dp))
         Column {
             Text(msg.author)
             Text(msg.body)
